35a71692d91ce7364a8c152e8a749fa0652fcfee
Arthur Soares Amorelli Dias <arthur-soares@hotmail.com>
2023-02-03 17:46:11 -0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -37,10 +37,10 @@ const Navbar = () => {
               <p><a href="#possibility">Open AI</a></p>
               <p><a href="#features">Case Studies</a></p>
               <p><a href="#blog">Library</a></p>
-            <div className="gpt3__navbar-menu_container-links-sign">
-              <p>Sign in</p>
-              <button type="button">Sign up</button>
-            </div> 
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div> 
             </div>
           </div>
         )}
